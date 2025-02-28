c02579db6b415176e52e8b8ae42a7290241acc78
Johan <hernandez.johan.6949@eam.edu.co>
2025-02-28 11:23:58 -0500

    PROGRAMADOR B Agregrado metodo C

--- src/prueba/Inventario.java
+++ src/prueba/Inventario.java
@@ -13,9 +13,13 @@ public class Inventario {
     public void metodoA() {
         System.out.println("Ejecutando método A");
     }
-    
+
     public void metodoB() {
- System.out.println("Ejecutando método B");
- }
+        System.out.println("Ejecutando método B");
+    }
+
+    public void metodoC() {
+        System.out.println("Ejecutando método C");
+    }
 
 }
